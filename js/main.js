b894e4beba1d060a261c8e18e6cc1c5edc054a6f
Julio Faria <juliofariadasilva@hotmail.com>
2022-08-11 17:26:33 -0300

    Integração

---
+++ js/main.js
@@ -0,0 +1,12 @@
+var nome = "Julio Faria";
+var idade = 33;
+var idade2 = 10;
+var frase = "Japão é o melhor time do mundo";
+
+
+console.log(nome);
+console.log(idade + idade2);
+console.log(frase.replace("Japão", "Brasil"));
+
+//alert(frase);
+//alert(idade + idade2);
