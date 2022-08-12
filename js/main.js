605bcbef45accfa491ad733da217c644275d9b17
Julio Faria <juliofariadasilva@hotmail.com>
2022-08-12 16:00:02 -0300

    Array e Dicionario

--- js/main.js
+++ js/main.js
@@ -1,12 +1,39 @@
-var nome = "Julio Faria";
-var idade = 33;
-var idade2 = 10;
-var frase = "Japão é o melhor time do mundo";
+var frutas = [{nome:"maça", cor:"vermelha"}, {nome:"uva", cor:"roxa"}]
+console.log(frutas);
+alert(frutas[1].nome);
 
 
-console.log(nome);
-console.log(idade + idade2);
-console.log(frase.replace("Japão", "Brasil"));
+
+/*
+var fruta = {nome:"maça", cor:"vermelha"}
+console.log(fruta.nome);
+alert(fruta.cor);
+*/
+
+
+//var lista = ["maça", "pera", "laranja"];
+//lista.push("uva");
+//lista.pop();
+
+//console.log(lista);
+//console.log(lista.toString);
+//console.log(lista.join(" - "));
+
+
+
+
+
+//var nome = "Julio Faria";
+//var idade = 33;
+//var idade2 = 10;
+//var frase = "Japão é o melhor time do mundo";
+
+
+//console.log(nome);
+//console.log(idade + idade2);
+//console.log(frase.replace("Japão", "Brasil"));
+
 
 //alert(frase);
 //alert(idade + idade2);
+//replace substitui o primeiro item pelo segundo;
